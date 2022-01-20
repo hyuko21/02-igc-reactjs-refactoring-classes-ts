a04563078251e13d11375186545d64a0a700f5f3
Victor Ribeiro <hyukodev@gmail.com>
2022-01-19 22:12:48 -0300

    refactor: migrate to ts

---
+++ src/@types/food.d.ts
@@ -0,0 +1,8 @@
+type Food = {
+  id: number
+  name: string
+  description: string
+  price: string
+  available: boolean
+  image: string
+}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,13 +4,19 @@ import {
   useState,
   useCallback,
 } from 'react';
+import { IconType } from 'react-icons';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string
+  icon?: IconType
+}
+
+export const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -47,7 +53,5 @@ const Input = ({ name, icon: Icon, ...rest }) => {
         {...rest}
       />
     </Container>
-  );
-};
-
-export default Input;
+  )
+}
